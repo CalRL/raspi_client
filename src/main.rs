a2661152159eb4cc534d3bc1fb7ebd5fcbc5fe86
Cal <43823915+CalRL@users.noreply.github.com>
2025-04-12 19:43:33 +0100

    added missing types

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
-use std::thread::current;
-use rppal::gpio::Gpio;
+use std::sync::{Arc, Mutex, MutexGuard};
+use rppal::gpio::{Gpio, OutputPin};
 const LED_PIN: u8 = 17;
-fn handle_client(mut stream: TcpStream, led: Arc<Mutex<rppal::gpio::OutputPin>>, state: Arc<Mutex<bool>>) {
-    let mut reader = BufReader::new(stream.try_clone().unwrap());
+fn handle_client(mut stream: TcpStream, led: Arc<Mutex<OutputPin>>, state: Arc<Mutex<bool>>) {
+    let mut reader: BufReader<TcpStream> = BufReader::new(stream.try_clone().unwrap());
     loop {
-        let mut buffer = String::new();
+        let mut buffer: String = String::new();
         if(reader.read_line(&mut buffer).unwrap() == 0) {
             break;
         }
 
-        let command = buffer.trim();
+        let command: &str= buffer.trim();
         println!("Received {}", command);
-        let mut pin = led.lock().unwrap();
-        let mut current_state = state.lock().unwrap();
+        let mut pin: MutexGuard<OutputPin> = led.lock().unwrap();
+        let mut current_state: MutexGuard<bool> = state.lock().unwrap();
 
-        let response = match command {
+        let response: String = match command {
             "toggle" => {
                 *current_state = !*current_state;
                 pin.write(if *current_state {
@@ -48,19 +47,19 @@ fn handle_client(mut stream: TcpStream, led: Arc<Mutex<rppal::gpio::OutputPin>>,
 }
 
 fn main() {
-    let gpio = Gpio::new().expect("Failed to access GPIO");
-    let led_pin = gpio.get(LED_PIN).unwrap().into_output();
+    let gpio: Gpio = Gpio::new().expect("Failed to access GPIO");
+    let led_pin: OutputPin = gpio.get(LED_PIN).unwrap().into_output();
 
-    let led = Arc::new(Mutex::new(led_pin));
-    let led_state = Arc::new(Mutex::new(false));
+    let led: Arc<Mutex<OutputPin>> = Arc::new(Mutex::new(led_pin));
+    let led_state: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
 
-    let listener = TcpListener::bind("0.0.0.0:8000").expect("Could not bind");
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:8000").expect("Could not bind");
     println!("Listening 0.0.0.0:8000");
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let led = Arc::clone(&led);
-                let state = Arc::clone(&led_state);
+                let led: Arc<Mutex<OutputPin>> = Arc::clone(&led);
+                let state : Arc<Mutex<bool>>= Arc::clone(&led_state);
                 std::thread::spawn(move || handle_client(stream, led, state));
             }
             Err(e) => eprintln!("Connection failed: {}", e),
